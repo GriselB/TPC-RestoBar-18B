bea843d2503242301ee41248a26f4a16a69766c2
GriselB <morales.grisel@outlook.com>
2026-05-31 22:15:00 -0300

    Se agrega la clase Insumo

---
+++ Dominio/Insumo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Insumo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public Categoria Categoria { get; set; }
+        public bool Activo { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
