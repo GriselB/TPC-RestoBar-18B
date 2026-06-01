4e8e20a5647b617d2b28c8fa0ffb94cb9871325b
GriselB <morales.grisel@outlook.com>
2026-05-31 22:18:00 -0300

    Se agrega la clase Pedido
    CUIDADO: Falta agregar las clases Mesa y Usuario para completar correctamente las referencias de la entidad.

---
+++ Dominio/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Dominio
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public Mesa Mesa { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime FechaApertura { get; set; }
+        public DateTime? FechaCierre { get; set; }
+        public bool Activo { get; set; }
+    }
+}
