3af0118d8f0e9da9d9d411d574edc52496c1f729
GriselB <morales.grisel@outlook.com>
2026-05-31 22:09:00 -0300

    Se agrega la entidad Menu

---
+++ Dominio/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Url { get; set; }
+        public string Modulo { get; set; }
+        public bool Activo { get; set; }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
