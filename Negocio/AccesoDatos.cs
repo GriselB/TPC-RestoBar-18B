c9d18c4efd93554346b84e221aad73c11de70481
GriselB <morales.grisel@outlook.com>
2026-06-05 00:02:12 -0300

    Creación de AccesoDatos.cs

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
+            comando = new SqlCommand();
+        }
+
+
+    }
+}
