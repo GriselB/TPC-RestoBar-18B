5532d56d29c219eb301eebb08d495230574c6cab
GriselB <morales.grisel@outlook.com>
2026-06-05 19:49:50 -0300

    Fix en ListaUsuario, en algún momento se perdió el archivo y se volvió a crear para que sea webform

--- RestoWeb/Usuarios/ListaUsuario.aspx.cs
+++ RestoWeb/Usuarios/ListaUsuario.aspx.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
 
 namespace RestoWeb.Usuarios
 {
