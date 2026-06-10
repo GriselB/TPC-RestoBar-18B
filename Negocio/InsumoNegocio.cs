022e20c2378e4c8439012ae1b58ea3321bb1fac0
aurelianomichilini <aurelianomichilini@hotmail.com>
2026-06-09 21:10:17 -0300

    Se crea la clase insumo negocio y se suma la funcionalidad para crear un nuevo insumo

---
+++ Negocio/InsumoNegocio.cs
@@ -0,0 +1,75 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class InsumoNegocio
+    {
+        public List<Insumo> listar()
+        {
+            List<Insumo> lista = new List<Insumo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Nombre, Descripcion, Precio,Stock,IdCategoria,Activo FROM INSUMOS WHERE Activo = 1");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Insumo aux = new Insumo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    aux.Stock = (int)datos.Lector["Stock"];
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                    aux.Activo = bool.Parse(datos.Lector["Activo"].ToString());
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregarInsumo(Insumo nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"INSERT INTO INSUMOS (Nombre, Descripcion,Precio,Stock,IdCategoria,Activo) VALUES (@Nombre, @Descripcion, @Precio, @Stock, @IdCategoria,@Activo)");
+
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.setearParametro("@Precio", nuevo.Precio);
+                datos.setearParametro("@Stock", nuevo.Stock);
+                datos.setearParametro("@IdCategoria", nuevo.Categoria.Id);
+                datos.setearParametro("@Activo", true);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
--- RestoWeb/Insumos/FormularioInsumos.aspx.cs
+++ RestoWeb/Insumos/FormularioInsumos.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
 
 namespace RestoWeb.Insumos
 {
@@ -13,13 +15,58 @@ namespace RestoWeb.Insumos
         {
             if (!IsPostBack)
             {
-               
+                cargarCategorias();
             }
+            
         }
+        private void cargarCategorias()
+        {
+            CategoriaNegocio negocio = new CategoriaNegocio();
 
+            ddlCategoria.DataSource = negocio.listar();
+            ddlCategoria.DataTextField = "Descripcion";
+            ddlCategoria.DataValueField = "Id";
+            ddlCategoria.DataBind();
+
+            ddlCategoria.Items.Insert(0, new ListItem("Seleccione una categoría", ""));
+        }
         protected void btnGuardar_Click(object sender, EventArgs e)
         {
-       
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                    throw new Exception("Debe ingresar un nombre.");
+
+                if (!decimal.TryParse(txtPrecio.Text, out decimal precio))
+                    throw new Exception("Debe ingresar un precio válido.");
+
+                if (!int.TryParse(txtStock.Text, out int stock))
+                    throw new Exception("Debe ingresar un stock válido.");
+
+                if (ddlCategoria.SelectedValue == "")
+                    throw new Exception("Debe seleccionar una categoría.");
+
+                Insumo nuevo = new Insumo();
+
+                nuevo.Nombre = txtNombre.Text.Trim();
+                nuevo.Descripcion = txtDescripcion.Text.Trim();
+                nuevo.Precio = precio;
+                nuevo.Stock = stock;
+
+                nuevo.Categoria = new Categoria();
+                nuevo.Categoria.Id = int.Parse(ddlCategoria.SelectedValue);
+
+                nuevo.Activo = chkActivo.Checked;
+
+                InsumoNegocio negocio = new InsumoNegocio();
+                negocio.agregarInsumo(nuevo);
+
+                Response.Redirect("ListaInsumos.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
         }
     }
