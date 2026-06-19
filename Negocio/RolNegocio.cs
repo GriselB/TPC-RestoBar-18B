d04c404bc9419ceb3c58e26095d28d3550fc12c3
Grisel Bonadies <morales.grisel@outlook.com>
2026-06-19 16:39:00 -0300

    Se crea la clase RolNegocio.cs con el método de listar
    Se crea la clase RolNegocio dentro del proyecto Negocio con el método listar, que trae todos los roles desde la tabla ROLES. Es necesaria para poder poblar el DropDownList de Rol en FormularioUsuario.

---
+++ Negocio/RolNegocio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Dominio;
+
+namespace Negocio
+{
+    public class RolNegocio
+    {
+        public List<Rol> listar()
+        {
+            List<Rol> lista = new List<Rol>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM ROLES");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Rol aux = new Rol();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //Va a ser necesario generar el Error.aspx que nos quedó pendiente, para que pueda devolver la excepción en caso de que falle
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
