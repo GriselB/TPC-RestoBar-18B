f544bedba173c92c2990b847ac4b6379956efbc2
Grisel Bonadies <morales.grisel@outlook.com>
2026-06-19 15:21:00 -0300

    Se elimina un archivo que quedó mal del repositorio + modificación en el método de listar
    Se modifica el método de listar, para que ahora acepte parametros. Se incluyó el parámetro opcional, si no recibe nada trae todas las mesas pero en caso de pasarle un id que sea mayor a 0 busca la mesa específica.

--- Negocio/MesaNegocio.cs
+++ Negocio/MesaNegocio.cs
@@ -9,14 +9,23 @@ namespace Negocio
 {
     public class MesaNegocio
     {
-        public List<Mesa> listar()
+        public List<Mesa> listar(int id = 0)
         {
             List<Mesa> lista = new List<Mesa>();
             AccesoDatos datos = new AccesoDatos();
 
             try
             {
-                datos.setearConsulta("SELECT Id, Numero, Descripcion, Activo FROM MESAS WHERE Activo = 1");
+                string consulta = "SELECT Id, Numero, Descripcion, Activo FROM MESAS WHERE Activo = 1";
+
+                if (id > 0)
+                    consulta += " AND Id = @Id";
+
+                datos.setearConsulta(consulta);
+
+                if (id > 0)
+                    datos.setearParametro("@Id", id);
+
                 datos.ejecutarLectura();
 
                 while (datos.Lector.Read())
@@ -33,6 +42,7 @@ namespace Negocio
             catch (Exception ex)
             {
                 throw ex;
+                //Va a ser necesario generar el Error.aspx que nos quedó pendiente, para que pueda devolver la excepción en caso de que falle
             }
             finally
             {
