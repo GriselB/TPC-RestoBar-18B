5bd5b398b436c6e32d943a5db6d6587f3583b868
aurelianomichilini <aurelianomichilini@hotmail.com>
2026-06-30 20:23:44 -0300

    Se crea la clase PedidoNegocio y se sube el primer metodo para abrir un pedido

---
+++ Negocio/PedidoNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class PedidoNegocio
+    {
+        public int abrirPedido(int idMesa, int idUsuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"
+                INSERT INTO PEDIDOS 
+                (
+                    IdMesa, 
+                    IdUsuario, 
+                    FechaApertura, 
+                    FechaCierre, 
+                    Activo
+                )
+                OUTPUT INSERTED.Id
+                VALUES 
+                (
+                    @idMesa, 
+                    @idUsuario, 
+                    GETDATE(), 
+                    NULL, 
+                    1
+                )
+            ");
+
+                datos.setearParametro("@idMesa", idMesa);
+                datos.setearParametro("@idUsuario", idUsuario);
+
+                return datos.ejecutarAccionScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
