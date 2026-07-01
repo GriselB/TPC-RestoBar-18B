6392a2aa4d45537be5c508eda0e8299c81d98c2e
GriselB <morales.grisel@outlook.com>
2026-07-01 01:15:00 -0300

    Se crea AsignacionNegocio con métodos listarVigentes y asignar

---
+++ Negocio/AsignacionNegocio.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Dominio;
+
+namespace Negocio
+{
+    public class AsignacionNegocio
+    {
+        public List<Asignacion> listarVigentes()
+        {
+            List<Asignacion> lista = new List<Asignacion>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"SELECT M.Id AS IdMesa, M.Numero, M.Descripcion,
+                                              A.Id AS IdAsignacion, A.IdUsuario,
+                                              U.Nombre, U.Apellido
+                                       FROM MESAS M
+                                       LEFT JOIN ASIGNACIONES A ON A.IdMesa = M.Id 
+                                            AND A.FechaCierre IS NULL 
+                                            AND A.Activo = 1
+                                       LEFT JOIN USUARIOS U ON U.Id = A.IdUsuario
+                                       WHERE M.Activo = 1");
+
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Asignacion aux = new Asignacion();
+
+                    aux.Mesa = new Mesa();
+                    aux.Mesa.Id = (int)datos.Lector["IdMesa"];
+                    aux.Mesa.Numero = (int)datos.Lector["Numero"];
+                    aux.Mesa.Descripcion = datos.Lector["Descripcion"] is DBNull ? "" : (string)datos.Lector["Descripcion"];
+
+                    if (!(datos.Lector["IdAsignacion"] is DBNull))
+                    {
+                        aux.Id = (int)datos.Lector["IdAsignacion"];
+                        aux.Usuario = new Usuario();
+                        aux.Usuario.Id = (int)datos.Lector["IdUsuario"];
+                        aux.Usuario.Nombre = (string)datos.Lector["Nombre"];
+                        aux.Usuario.Apellido = (string)datos.Lector["Apellido"];
+                    }
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void asignar(int idMesa, int idUsuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"UPDATE ASIGNACIONES SET FechaCierre = @FechaCierre, Activo = 0
+                                       WHERE IdMesa = @IdMesa 
+                                       AND FechaCierre IS NULL 
+                                       AND Activo = 1");
+
+                datos.setearParametro("@FechaCierre", DateTime.Now);
+                datos.setearParametro("@IdMesa", idMesa);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            AccesoDatos datos2 = new AccesoDatos();
+
+            try
+            {
+                datos2.setearConsulta(@"INSERT INTO ASIGNACIONES (IdMesa, IdUsuario, FechaAsignacion, Activo)
+                                        VALUES (@IdMesa, @IdUsuario, @FechaAsignacion, 1)");
+
+                datos2.setearParametro("@IdMesa", idMesa);
+                datos2.setearParametro("@IdUsuario", idUsuario);
+                datos2.setearParametro("@FechaAsignacion", DateTime.Now);
+                datos2.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos2.cerrarConexion();
+            }
+        }
+    }
+}
