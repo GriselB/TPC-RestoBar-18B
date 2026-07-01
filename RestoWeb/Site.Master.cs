18af0474b1bd4ba317314d57743b3da9ff9ea49a
GriselB <morales.grisel@outlook.com>
2026-07-01 03:17:00 -0300

    Se agrega verificación de sesión en Site.Master y se muestra el nombre del usuario logueado

--- RestoWeb/Site.Master.cs
+++ RestoWeb/Site.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Web.UI;
 
 namespace RestoWeb
@@ -7,6 +9,16 @@ namespace RestoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!(Page is Login))
+            {
+                if (!Seguridad.sessionActiva(Session["usuario"]))
+                    Response.Redirect("~/Login.aspx", false);
+                else
+                {
+                    Usuario usuario = (Usuario)Session["usuario"];
+                    lblUsuario.Text = usuario.NombreCompleto;
+                }
+            }
         }
 
         protected void btnSalir_Click(object sender, EventArgs e)
