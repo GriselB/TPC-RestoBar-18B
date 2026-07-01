93a015e23d59e4070d192c5839acb46808c748a5
GriselB <morales.grisel@outlook.com>
2026-07-01 03:02:00 -0300

    Se conecta el Login con UsuarioNegocio.login

--- RestoWeb/Login.aspx.cs
+++ RestoWeb/Login.aspx.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web.UI;
 using Dominio;
 using Negocio;
 
@@ -9,10 +8,43 @@ namespace RestoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["usuario"] != null)
+                Response.Redirect("Default.aspx", false);
         }
 
         protected void btnIngresar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombreUsuario.Text))
+                    throw new Exception("El campo Usuario es obligatorio.");
+
+                if (string.IsNullOrWhiteSpace(txtPass.Text))
+                    throw new Exception("El campo Contraseña es obligatorio.");
+
+                Usuario usuario = new Usuario();
+                usuario.NombreUsuario = txtNombreUsuario.Text;
+                usuario.Pass = txtPass.Text;
+
+                UsuarioNegocio negocio = new UsuarioNegocio();
+                Usuario resultado = negocio.login(usuario);
+
+                if (resultado != null)
+                {
+                    Session.Add("usuario", resultado);
+                    Response.Redirect("Default.aspx", false);
+                }
+                else
+                {
+                    lblError.Text = "Usuario o contraseña incorrectos.";
+                    lblError.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.Visible = true;
+            }
         }
     }
 }
