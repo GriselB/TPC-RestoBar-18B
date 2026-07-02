9ea80ac2085ffa1a0731b771768a8538be0bbeba
GriselB <morales.grisel@outlook.com>
2026-07-02 01:16:29 -0300

    Se crea el archivo restobar.js para llevar todo lo realizado en javascript allí, se incluye el script que se utiliza en default para mostrar el modal

---
+++ RestoWeb/Scripts/restobar.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    var hfMostrarModal = document.querySelector('.hf-mostrar-modal');
+    if (hfMostrarModal && hfMostrarModal.value === '1') {
+        var modalElement = document.getElementById('modalConfirmarPedido');
+        if (modalElement) {
+            var modal = new bootstrap.Modal(modalElement);
+            modal.show();
+        }
+    }
+
+});
